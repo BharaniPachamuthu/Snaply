dcc76ce00885b001c91725174650a50e2363d289
Bharani_official <163810588+BharaniPachamuthu@users.noreply.github.com>
2024-12-20 10:46:52 +0530

    Update splashscreen.dart

--- lib/module_homescreen/splashscreen.dart
+++ lib/module_homescreen/splashscreen.dart
@@ -20,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
       const Duration(seconds: 3),
           () => Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const HomePages()),
+        MaterialPageRoute(builder: (context) => const HomePage()),
       ),
     );
   }
